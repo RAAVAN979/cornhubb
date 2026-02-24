1eb587d49f39b79a9495e8f0c54980b4a93db025
RAAVAN979 <107475456+RAAVAN979@users.noreply.github.com>
2026-02-24 16:10:00 +0000

    Add form handling for user account creation

--- script.js
+++ script.js
@@ -10,7 +10,56 @@ const Yes = document.getElementById('Yes');
 
             const CreateAcc = document.getElementById('CreateAcc');
 
+            const Age = document.getElementById('Age');
+            const FullName = document.getElementById('Fullname');
+            const Email = document.getElementById('Email');
+            const Password = document.getElementById('Password');
+            const CEmail = document.getElementById('Cemail');
+            const Cpassword = document.getElementById('Cpassword');
 
+            let Storage = JSON.parse(localStorage.getItem('users')) || [];
+
+
+            document.getElementById('CreateAccount').addEventListener('click', () =>
+            {
+                if ( 
+                    FullName.value.trim() === "" ||
+                    Age.value.trim() === "" ||
+                    Email.value.trim() === "" ||
+                    Password.value.trim() === "" 
+                ) {  document.getElementById('message').style.display = "flex";
+                     document.getElementById('message').textContent = 'Please fill in all field';
+                      document.getElementById('message').style.color = "red";
+                      document.getElementById('message').style.top = '15%';
+                    document.getElementById('CreateAccount').pointerEvents = "none";
+                
+                } else{
+
+                const user = {
+                    name: FullName.value,
+                    age: Age.value,
+                    password: Password.value,
+                    email: Email.value,
+                };
+
+                document.getElementById('CreateAccount').pointerEvents = 'auto';
+
+                Storage.push(user);
+                localStorage.setItem('users', JSON.stringify(Storage));
+
+                document.getElementById('message').style.display = "flex";
+                Home.style.display = 'none';
+                LoginForm.style.display = 'none';
+                CreateAcc.style.display = "none";
+
+                setTimeout(() => {
+                    document.getElementById('message').style.display = "none";
+                     Home.style.display = 'flex';
+                LoginForm.style.display = 'none';
+                 document.getElementById('AgeVerify').style.display = 'none';
+                
+                }, 10000);
+            } }); 
 
             Yes.addEventListener('click',  () => {
                 document.getElementById("Home").style.filter = 'blur(0)';
@@ -39,7 +88,8 @@ const Yes = document.getElementById('Yes');
             CreateBtn.addEventListener('click', () =>
             {
                 Home.style.display = "none";
-                
                 CreateAcc.style.display = " flex";
                 LoginForm.style.display = "none";
             });
+
+            
